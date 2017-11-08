80d0246ccec1f7caa3e18dc140844142693d4a5d
syed abuthahir <abuvanth@gmail.com>
2017-11-08 14:07:06 +0530

    Update result.py
    csv file download

--- result.py
+++ result.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
-#u=913013104001  #starting Register  number
+import csv
 u=int(raw_input("Enter Your starting Reg No "))
 n=int(raw_input("Enter Your Ending Reg No "))
 while  u<=n:  #ending Register number
        r=requests.post('http://aucoe.annauniv.edu/cgi-bin/result/cgrade.pl',data={'regno':u})
-#print r.content
        u=u+1
        s=bs(r.content,'html.parser')
+       a=[]
+       f=open('audata.csv','a+')
+       writer=csv.writer(f)
+       a=[]
        for st in s.find_all('strong'):
-           print st.string
-           stra=str(st.string)
-           stra=stra+'\n'
-           with open('result.txt','a') as txtfile:
-                txtfile.write(stra)
+           a.append(st.text)
+       if len(a)!=6:
+          while a:
+               writer.writerow(a[:3])
+               a=a[3:]
