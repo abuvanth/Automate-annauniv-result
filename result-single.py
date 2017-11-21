b9b7794c4a8582ad9f8c41c03169ff1adee28cce
syed abuthahir <abuvanth@gmail.com>
2017-11-21 21:59:00 +0530

    Create result-single.py

---
+++ result-single.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+u=int(raw_input("Enter Your Reg No "))
+r=requests.post('http://aucoe.annauniv.edu/cgi-bin/result/cgrade.pl',data={'regno':u})
+s=bs(r.content,'html.parser')
+a=[]
+f=open('audata.csv','a+')
+writer=csv.writer(f)
+a=[]
+for st in s.find_all('strong'):
+       a.append(st.text)
+if len(a)!=6:
+   while a:
+         writer.writerow(a[:3])
+         a=a[3:]
